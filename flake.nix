68ff33352928f36c432e43086b3059683624dcd6
DotRedstone <2724236945@qq.com>
2026-06-03 18:03:24 +0800

    Initial commit of firmware including DSP blobs

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Firmware for Xiaomi Pad 6S Pro (sheng), including DSP sensor blobs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Assumes evaluating from x86_64-linux, but the firmware itself is arch-independent
+  in {
+    packages.aarch64-linux.default = pkgs.stdenvNoCC.mkDerivation {
+      pname = "sheng-firmware-full";
+      version = "1.0.0";
+
+      src = ./.;
+
+      dontFixup = true;
+      dontStrip = true;
+      dontBuild = true;
+
+      installPhase = ''
+        runHook preInstall
+
+        # Create standard directories
+        mkdir -p $out/lib/firmware/qcom/sm8550/sheng
+        mkdir -p $out/lib/firmware/rfsa/adsp
+        mkdir -p $out/bin
+        mkdir -p $out/lib64
+        mkdir -p $out/etc/sensors
+
+        # 1. Base firmware
+        if [ -d "qcom" ]; then
+            cp -r qcom $out/lib/firmware/
+        fi
+
+        # 2. ADSP / Hexagon DSP blobs
+        if [ -d "firmware/rfsa/adsp" ]; then
+            cp -r firmware/rfsa/adsp/* $out/lib/firmware/rfsa/adsp/
+        fi
+
+        # 3. Executables
+        if [ -d "bin" ]; then
+            cp -r bin/* $out/bin/
+            chmod +x $out/bin/adsprpcd
+        fi
+
+        # 4. Libraries
+        # Note: Bionic Android libs are not directly usable by glibc, but we ship them just in case they are needed for hybris/proot.
+        if [ -d "lib64" ]; then
+            cp -r lib64/* $out/lib64/
+        fi
+        if [ -d "lib" ]; then
+            # We skip copying the entire /vendor/lib to avoid conflicting with Linux's /lib structure,
+            # or we can place them in a safe vendor path. We'll copy them to /usr/lib/android-vendor/lib
+            mkdir -p $out/usr/lib/android-vendor/lib
+            cp -r lib/* $out/usr/lib/android-vendor/lib/
+        fi
+
+        # 5. Configurations
+        if [ -d "etc/sensors" ]; then
+            cp -r etc/sensors/* $out/etc/sensors/
+        fi
+
+        runHook postInstall
+      '';
+    };
+
+    # Also expose it for cross-compilation evaluation
+    packages.x86_64-linux.default = self.packages.aarch64-linux.default;
+  };
+}
